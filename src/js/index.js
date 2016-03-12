4f9780d9f26cef334565198df9009f89dc53ec3e
Ben Lee <leeohsunshine@gmail.com>
2016-03-12 12:58:42 -0500

    Changed positioning, colors, hover attributes, and alignments on many elements.

--- src/js/index.js
+++ src/js/index.js
@@ -9,16 +9,22 @@ var productDisplay = items.map(function(obj){
 	var prodTitle = obj.title
 	var prodPrice = obj.price
 	var shopName = obj.Shop.shop_name
+	var currCode = obj.currency_code
 		return `<div class="prodInfo">
+					<img id="heart" src="./css/heart.png">
+					<img id="hamburger" src="./css/hamburger.png">
+					<div class="triangle_holder">
+					<div class='triangle'></div>
+					</div>
 					<img src=${imgURL}>
 					<div class="shop">
-						<span class="descr">${prodTitle} 
+						<span class="title">${prodTitle} 
 						<span>${shopName}</span>
 						</span>
 					</div>
 					<span class="price_contain">
 					<span class="shopName">${shopName}</span>
-					<span class="price">${prodPrice}</span>
+					<span class="price">${prodPrice} ${currCode}</span>
 					</span>
 				</div>`
 	})
@@ -26,3 +32,6 @@ var productDisplay = items.map(function(obj){
 var main2 = $(".main2wrapper")
 
 main2.append(productDisplay)
+
+var hamburger = $("#hamburger")
+var heart = $("#heart")
